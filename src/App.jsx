f95c20fd487717012013434e61320019930a3cb3
Maxim <maksrabb135@gmail.com>
2025-04-28 18:34:25 +0500

    я создал познаю React JS, Java Spring и PostgreSQL на примере курсовой по МиСПИСиТ

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Home from './pages/Home'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      {/* пока что все компоненты в Home, потом ебану
+      страницы... как-нибудь */}
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+    <>
+    <h1>Ну, поехали</h1>
+    </>
+    )
+}
+
+export default Home;
